3ae9c2d478a08d2faed7b9f453f96fc01b1a053b
elifyilmaz00 <elifyilmaz1906@gmail.com>
2025-11-18 19:16:53 +0300

    feat(oop): Instantiate User class and test core methods

--- script.js
+++ script.js
@@ -30,3 +30,23 @@ class Kullanici{
         console.log(`${this.ad} çıkış yaptı.`);
     }
 }
+
+//2. NESNE OLUŞTURMA (Kalıptan Üretim)
+//"new" anahtar kelimesi, constructor'ı tetikler.
+
+const kullanici1 = new Kullanici("Amy", "amiy@example.com");
+const kullanici2 = new Kullanici("Lexa", "lexa@example.com");
+
+//3. NESNELERİ KULLANMA
+//Nesnelerin özelliklerine ve metotlarına erişebiliriz.
+
+kullanici1.puanKazan(); //Amy 1 puan kazandı yeni puan: 1
+kullanici1.puanKazan(); //Amy 1 puan kazandı yeni puan: 2
+kullanici1.cikisYap(); //Amy çıkış yaptı.
+
+kullanici2.puanKazan(); //Lexa 1 puan kazandı yeni puan: 1
+kullanici2.cikisYap(); //Lexa çıkış yaptı.
+
+//Metot zincirleme örneği
+
+kullanici1.puanKazan().puanKazan().cikisYap();//Puanı 3 kez art arda arttırır.
